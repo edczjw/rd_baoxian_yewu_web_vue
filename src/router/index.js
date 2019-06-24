912319c3f72698eb824eff6882d1f91d5a9750dd
edczjw <347350971@qq.com>
2019-06-24 11:27:09 +0800

    ==

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      // 登录
+      path: '/login',
+      name: '登录',
+      component: require('../view/login/login.vue').default,
+    },{
+      // 主页
+      path: '/home',
+      name: '空白主页',
+      component: require('../view/index/index.vue').default,
+      // 含有文件夹的文件children
+      children:[{
+        // 主题home
+          path: '/mshome',
+          name: '主题主页',
+          component: require('../view/index/home.vue').default,
+          meta: {
+            requireAuth: true
+          }
+        }]
+      }]
+})
